0a2cf82d8ee9a4f22bc03513abb421691979c74b
Daniel Paggiola <dpaggiola@gmail.com>
2022-09-17 23:08:32 -0300

    Clase 8 finished.

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace platzi_asp_net_core.Controllers;
+
+public class EscuelaController: Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
